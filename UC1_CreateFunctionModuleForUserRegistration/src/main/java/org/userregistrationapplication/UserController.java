a8444cf303a09ab7a399f83e0f22aa3e774d0b2d
imprathamraj <rjpratham05@gmail.com>
2025-02-20 23:18:10 +0530

    [Pratham Raj] Add. UserController

---
+++ UC1_CreateFunctionModuleForUserRegistration/src/main/java/org/userregistrationapplication/UserController.java
@@ -0,0 +1,22 @@
+package org.userregistrationapplication;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// User Controller
+@RestController
+@RequestMapping("/api/users")
+class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
