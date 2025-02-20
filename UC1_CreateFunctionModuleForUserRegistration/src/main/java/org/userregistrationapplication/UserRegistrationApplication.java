9f088b282387cf9dccb20d83b96a48cc6188f555
imprathamraj <rjpratham05@gmail.com>
2025-02-20 23:18:30 +0530

    [Pratham Raj] Add. UserRegistrationApplication

---
+++ UC1_CreateFunctionModuleForUserRegistration/src/main/java/org/userregistrationapplication/UserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package org.userregistrationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// Main Application
+@SpringBootApplication
+public class UserRegistrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationApplication.class, args);
+    }
+}
